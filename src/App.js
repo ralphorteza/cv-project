ddf75545d886d8a240e0f8a7685273a6b69be042
ralph orteza <ralphlorteza@gmail.com>
2023-03-03 14:34:18 -0800

    Migrate App children into CVPreview

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-import GeneralInfo from "./components/form/GeneralInfo";
-import EducationInfo from "./components/form/EducationInfo";
-import ExperienceInfo from "./components/form/ExperienceInfo";
 import CVPreview from "./components/CVPreview";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 import "./App.css";
 
 export default function App() {
@@ -13,103 +10,15 @@ export default function App() {
     () => JSON.parse(localStorage.getItem("data")) || []
   );
 
-  function handleData() {
-
-  }
+  function handleData() {}
 
   React.useEffect(() => {
     localStorage.setItem("data", JSON.stringify(data))
   }, [data]);
-
-  const [genInfo, setGenInfo] = React.useState({
-    firstName: "",
-    lastName: "",
-    address: "",
-    phone: "",
-    email: "",
-  });
-
-  function handleGenInfoChange(e) {
-    // console.log(e);
-    const {value, name} = e.target
-    // console.log(`value: ${value}, name:${name}`)
-    setGenInfo( prevInfo => {
-      return {
-        ...prevInfo,
-        [name]: value
-      }
-    })
-  }
-
-
-  const [eduInfo, setEduInfo] = React.useState({
-    university:"",
-    location:"",
-    degree:"",
-    from:"",
-    to:"",
-  });
-
-  function handleEduInfoChange(e) {
-    // console.log(e);
-    const {name, value} = e.target;
-
-    setEduInfo(prevInfo => {
-      return {
-        ...prevInfo,
-        [name]: value
-      }
-    })
-  }
-
-  const [experienceInfo, setExperienceInfo] = React.useState({
-    company:"",
-    location:"",
-    position:"",
-    from:"",
-    to:"",
-  });
-
-  function handleExperienceInfoChange(e) {
-    // console.log(e);
-    const {name, value} = e.target;
-
-    setExperienceInfo(prevInfo => {
-      return {
-        ...prevInfo,
-        [name]: value
-      }
-    })
-  }
-
-  // const cvData = {
-  //   genInfo,
-  //   eduInfo,
-  //   experienceInfo,
-  // };
-
-  // console.log(cvData)
   
   return (
     <div>
-      <h3>General Information</h3>
-      <GeneralInfo
-        handleGenInfoChange={handleGenInfoChange}
-        genInfo={genInfo}
-      />
-      <h3>Education</h3>
-      <EducationInfo
-        handleEduInfoChange={handleEduInfoChange}
-        eduInfo={eduInfo}
-      />
-      <h3>Experience</h3>
-      <ExperienceInfo
-        handleExperienceInfoChange={handleExperienceInfoChange}
-        experienceInfo={experienceInfo}
-      />
-
-      <h2>Preview</h2>
-      <CVPreview/>
+      <CVPreview />
     </div>
   );
 }
--- src/components/CVPreview.js
+++ src/components/CVPreview.js
@@ -1,9 +1,88 @@
 import React from "react"
+import GeneralInfo from "./form/GeneralInfo";
+import EducationInfo from "./form/EducationInfo";
+import ExperienceInfo from "./form/ExperienceInfo";
 
 export default function CVPreview(props) {
+
+  const [genInfo, setGenInfo] = React.useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    phone: "",
+    email: "",
+  });
+
+  function handleGenInfoChange(e) {
+    // console.log(e);
+    const {value, name} = e.target
+    // console.log(`value: ${value}, name:${name}`)
+    setGenInfo( prevInfo => {
+      return {
+        ...prevInfo,
+        [name]: value
+      }
+    })
+  }
+
+
+  const [eduInfo, setEduInfo] = React.useState({
+    university:"",
+    location:"",
+    degree:"",
+    from:"",
+    to:"",
+  });
+
+  function handleEduInfoChange(e) {
+    // console.log(e);
+    const {name, value} = e.target;
+
+    setEduInfo(prevInfo => {
+      return {
+        ...prevInfo,
+        [name]: value
+      }
+    })
+  }
+
+  const [experienceInfo, setExperienceInfo] = React.useState({
+    company:"",
+    location:"",
+    position:"",
+    from:"",
+    to:"",
+  });
+
+  function handleExperienceInfoChange(e) {
+    // console.log(e);
+    const {name, value} = e.target;
+
+    setExperienceInfo(prevInfo => {
+      return {
+        ...prevInfo,
+        [name]: value
+      }
+    })
+  }
+
   return (
     <div className="cv-preview">
-      
+            <h3>General Information</h3>
+      <GeneralInfo
+        handleGenInfoChange={handleGenInfoChange}
+        genInfo={genInfo}
+      />
+      <h3>Education</h3>
+      <EducationInfo
+        handleEduInfoChange={handleEduInfoChange}
+        eduInfo={eduInfo}
+      />
+      <h3>Experience</h3>
+      <ExperienceInfo
+        handleExperienceInfoChange={handleExperienceInfoChange}
+        experienceInfo={experienceInfo}
+      />
     </div>
   );
 }
