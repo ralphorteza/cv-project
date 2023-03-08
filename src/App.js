a727d9b95f4d633289128ac4d450195bdb045b57
ralph orteza <ralphlorteza@gmail.com>
2023-03-08 12:49:04 -0800

    Utilize nanoid for id generation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import FormOverview from "./components/FormOverview";
-// import { nanoid } from "nanoid";
+import { nanoid } from "nanoid";
 import "./App.css";
 
 export default function App() {
   const initialState = [
-    {category:"general", id:"001", firstName:"", lastName:"", address:"", phone:"", email:""},
-    {category:"education", id:"002", university:"", location:"", degree:"", from:"", to:""},
-    {category:"experience", id:"003", company:"", location:"", position:"", from:"", to:""}
+    {category:"general", id: nanoid(), firstName:"", lastName:"", address:"", phone:"", email:""},
+    {category:"education", id: nanoid(), university:"", location:"", degree:"", from:"", to:""},
+    {category:"experience", id: nanoid(), company:"", location:"", position:"", from:"", to:""}
   ]
   /* Find a way to have multiple instances of ExperienceInfo */
   const [data, setData] = React.useState(
@@ -19,34 +19,38 @@ export default function App() {
   }, [data]);
 
   function handleData(e) {
-    // console.log(e.id)
-    const obj = e;
-
-    if (data.length === 0) {
-      return setData(prevData => {
-        return [obj, ...prevData];
-      });
-    }
-
-    if (data.some( element => element.id === obj.id)) {
-      return setData(prevData => {
-        const array = [];
-        prevData.forEach( prevElem => {
-          if (prevElem.id === obj.id) {
-            array.push(obj);
-          } else {
-            array.push(prevElem);
-          }
-        });
-        return array;
-      });
-    } else {
-      return setData(prevData => {
-        return [obj, ...prevData];
-      });
-    }
 
   }
+
+  // function handleData(e) {
+  //   // console.log(e.id)
+  //   const obj = e;
+
+  //   if (data.length === 0) {
+  //     return setData(prevData => {
+  //       return [obj, ...prevData];
+  //     });
+  //   }
+
+  //   if (data.some( element => element.id === obj.id)) {
+  //     return setData(prevData => {
+  //       const array = [];
+  //       prevData.forEach( prevElem => {
+  //         if (prevElem.id === obj.id) {
+  //           array.push(obj);
+  //         } else {
+  //           array.push(prevElem);
+  //         }
+  //       });
+  //       return array;
+  //     });
+  //   } else {
+  //     return setData(prevData => {
+  //       return [obj, ...prevData];
+  //     });
+  //   }
+
+  // }
 
   return (
     <div>
--- src/components/FormOverview.js
+++ src/components/FormOverview.js
@@ -3,71 +3,81 @@ import GeneralInfo from "./form/GeneralInfo";
 import EducationInfo from "./form/EducationInfo";
 import ExperienceInfo from "./form/ExperienceInfo";
 
-export default function CVPreview(props) {
-  const {data, handleData} = props;
-
-  const [genInfo, setGenInfo] = React.useState(data[0]);
-  
-  function handleGenInfoChange(e) {
-    const {value, name} = e.target;
-
-    setGenInfo(prevGenInfo => {
-      return {
-        ...prevGenInfo,
-        [name]: value
-      }
-    });
-
-    handleData(genInfo);
-  }
-
-  const [eduInfo, setEduInfo] = React.useState(data[1]);
-
-  function handleEduInfoChange(e) {
-    const {name, value} = e.target;
-
-    setEduInfo(prevInfo => {
-      return {
-        ...prevInfo,
-        [name]: value
-      }
-    });
-
-    handleData(eduInfo);
-  }
-
-  const [experienceInfo, setExperienceInfo] = React.useState(data[2]);
-  function handleExperienceInfoChange(e) {
-    // console.log(e);
-    const {name, value} = e.target;
-
-    setExperienceInfo(prevInfo => {
-      return {
-        ...prevInfo,
-        [name]: value
-      }
-    });
-
-    handleData(experienceInfo);
-  }
+export default function FormOverview(props) {
 
   return (
-    <div className="cv-preview">
-            <h3>General Information</h3>
-      <GeneralInfo
-        handleGenInfoChange={handleGenInfoChange}
-        genInfo={genInfo}
-      />
-      <h3>Education</h3>
-      <EducationInfo
-        handleEduInfoChange={handleEduInfoChange}
-        eduInfo={eduInfo}
-      />
-      <h3>Experience</h3>
-      <ExperienceInfo
-        handleExperienceInfoChange={handleExperienceInfoChange}
-        experienceInfo={experienceInfo}
-      />
+    <div>
+      <h2>General</h2>
+      <h2>Education</h2>
+      <h2>Experience</h2>
     </div>
   );
 }
+// export default function CVPreview(props) {
+//   const {data, handleData} = props;
+
+//   const [genInfo, setGenInfo] = React.useState(data[0]);
+  
+//   function handleGenInfoChange(e) {
+//     const {value, name} = e.target;
+
+//     setGenInfo(prevGenInfo => {
+//       return {
+//         ...prevGenInfo,
+//         [name]: value
+//       }
+//     });
+
+//     handleData(genInfo);
+//   }
+
+//   const [eduInfo, setEduInfo] = React.useState(data[1]);
+
+//   function handleEduInfoChange(e) {
+//     const {name, value} = e.target;
+
+//     setEduInfo(prevInfo => {
+//       return {
+//         ...prevInfo,
+//         [name]: value
+//       }
+//     });
+
+//     handleData(eduInfo);
+//   }
+
+//   const [experienceInfo, setExperienceInfo] = React.useState(data[2]);
+//   function handleExperienceInfoChange(e) {
+//     // console.log(e);
+//     const {name, value} = e.target;
+
+//     setExperienceInfo(prevInfo => {
+//       return {
+//         ...prevInfo,
+//         [name]: value
+//       }
+//     });
+
+//     handleData(experienceInfo);
+//   }
+
+//   return (
+//     <div className="cv-preview">
+//             <h3>General Information</h3>
+//       <GeneralInfo
+//         handleGenInfoChange={handleGenInfoChange}
+//         genInfo={genInfo}
+//       />
+//       <h3>Education</h3>
+//       <EducationInfo
+//         handleEduInfoChange={handleEduInfoChange}
+//         eduInfo={eduInfo}
+//       />
+//       <h3>Experience</h3>
+//       <ExperienceInfo
+//         handleExperienceInfoChange={handleExperienceInfoChange}
+//         experienceInfo={experienceInfo}
+//       />
+//     </div>
+//   );
+// }
